780d2faae9283321d1c2bc7d288f4409aeb8f5a3
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2025-07-29 19:21:36 +0200

    Update output.tf

--- petclinic-infra/terraform/output.tf
+++ petclinic-infra/terraform/output.tf
@@ -1,49 +1,55 @@
 output "vpc_id" {
-  description = "The ID of the VPC"  # Provides the ID of the VPC for referencing in other tools.
+  description = "The ID of the VPC"
   value       = aws_vpc.main.id
 }
 
-output "subnet_id" {
-  description = "The ID of the subnet"  # Provides the ID of the subnet for routing or placement.
-  value       = aws_subnet.main.id
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
 }
 
-output "security_group_id" {
-  description = "The ID of the Kubernetes security group"  # Helps reference network rules.
-  value       = aws_security_group.k8s_sg.id
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.id
 }
+
+# Uncomment and adjust the security group output once you declare a security group resource.
+# output "security_group_id" {
+#   description = "The ID of the Kubernetes security group"
+#   value       = aws_security_group.k8s_sg.id
+# }
 
 output "bastion_public_ip" {
-  description = "Public IP of the bastion host"  # Used for external SSH access.
+  description = "Public IP of the bastion host"
   value       = aws_instance.bastion.public_ip
 }
 
 output "bastion_private_ip" {
-  description = "Private IP of the bastion host"  # Used for internal network access.
+  description = "Private IP of the bastion host"
   value       = aws_instance.bastion.private_ip
 }
 
 output "master_private_ip" {
-  description = "Private IP of the Kubernetes master node"  # Required for Kubernetes setup.
+  description = "Private IP of the Kubernetes master node"
   value       = aws_instance.master.private_ip
 }
 
 output "worker_private_ips" {
-  description = "List of private IPs of the Kubernetes worker nodes"  # Allows joining the cluster.
+  description = "List of private IPs of the Kubernetes worker nodes"
   value       = [for instance in aws_instance.worker : instance.private_ip]
 }
 
 output "bastion_instance_id" {
-  description = "Instance ID of the bastion host"  # Useful for automation and tracking.
+  description = "Instance ID of the bastion host"
   value       = aws_instance.bastion.id
 }
 
 output "master_instance_id" {
-  description = "Instance ID of the master node"  # For tracking and applying logic.
+  description = "Instance ID of the master node"
   value       = aws_instance.master.id
 }
 
 output "worker_instance_ids" {
-  description = "List of instance IDs of the worker nodes"  # Useful for monitoring.
+  description = "List of instance IDs of the worker nodes"
   value       = [for instance in aws_instance.worker : instance.id]
 }
